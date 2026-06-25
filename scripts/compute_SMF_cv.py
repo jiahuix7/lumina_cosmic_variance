9ae04a48928a50ecca9214bcb3906f9dd25200d4
Jiahui Xie <jxie2@mit.edu>
2026-06-25 17:15:46 -0400

    edits to SMF and comparison to observational data

--- scripts/compute_SMF_cv.py
+++ scripts/compute_SMF_cv.py
@@ -1,4 +1,3 @@
-# %%
 import sys
 from pathlib import Path
 
@@ -9,7 +8,7 @@ from astropy.cosmology import FlatLambdaCDM
 import h5py
 from scipy.integrate import quad
 
-repo_root = Path.cwd().parent
+repo_root = Path.cwd()
 sys.path.insert(0, str(repo_root))
 from scripts import groupcat
 
@@ -27,7 +26,8 @@ lumina_cosmology=FlatLambdaCDM(
 survey_area=9.7*u.arcmin**2
 delta_z=1.0
 mass_bin_width=0.5
-res_cut=20
+res_cut=0
+stellar_mass_cut=1e7
 geometry="cube"
 
 def calculate_field_volume(cosmology, z_center, delta_z, survey_area):
@@ -50,7 +50,7 @@ def calculate_field_volume(cosmology, z_center, delta_z, survey_area):
 
 def load_data(base_path, snap_num):
     catalog_path = groupcat.gcPath(base_path, snap_num)
-    halos = groupcat.loadHalos(base_path,snap_num,fields=["GroupPos", "Group_M_Crit200", "GroupLen"],)
+    subhalos = groupcat.loadSubhalos(base_path,snap_num,fields=["SubhaloPos", "SubhaloMassInRadType", "SubhaloLenType"],)
     with h5py.File(catalog_path, "r") as f:
         h = f["Parameters"].attrs["HubbleParam"]
         box_size_raw = f["Header"].attrs["BoxSize"]
@@ -62,58 +62,59 @@ def load_data(base_path, snap_num):
     print("box size:", box_size_cmpc, "cMpc")
 
     return {
-        "positions_raw": halos["GroupPos"],
-        "m200c_raw": halos["Group_M_Crit200"],
-        "particle_counts": halos["GroupLen"],
+        "positions_raw": subhalos["SubhaloPos"],
+        "can_mass_raw": subhalos["SubhaloMassInRadType"][:,4],
+        "particle_counts": subhalos["SubhaloLenType"][:,4],
         "h": h,
         "redshift": redshift,
         "box_size_cmpc": box_size_cmpc,
         "catalog_path": catalog_path,
     }
 
-def select_valid_halos(data, res_cut):
+def select_valid_subhalos(data, res_cut, stellar_mass_cut):
     positions = data["positions_raw"]
-    m200c = data["m200c_raw"]
+    mass = data["can_mass_raw"]
     particle_counts = data["particle_counts"]
     h = data["h"]
+    mass_msun = mass * 1e10 / h
 
-    valid = (
-        (particle_counts >= res_cut)
-        & (m200c > 0)
-        & np.isfinite(m200c)
-        & np.all(np.isfinite(positions), axis=1)
-    )
+    print(mass[np.logical_not(np.isfinite(mass))])
+    print(mass[np.logical_not(np.all(np.isfinite(positions), axis=1))])
+    print(mass[np.logical_not(mass > 0)])
+    print(len(mass[np.logical_not(mass > 0)]))
+    
 
+    valid = ((mass_msun >= stellar_mass_cut) & (particle_counts >= res_cut)& (mass > 0)& np.isfinite(mass)& np.all(np.isfinite(positions), axis=1))
+    mass_msun_ok=mass_msun[valid]
     positions_cmpc = positions[valid] / (1000 * h)
-    m200c_msun = m200c[valid] * 1e10 / h
 
-    print("All FoF groups:", len(m200c))
-    print("Selected halos:", valid.sum())
+    print("All subhalos:", len(mass))
+    print("Selected subhalos:", valid.sum())
     print("Selected fraction:", valid.mean())
 
-    return positions_cmpc, m200c_msun, valid
+    return positions_cmpc, mass_msun_ok, valid
 
-def assign_halos_to_fields(
+def assign_subhalos_to_fields(
     positions_cmpc,
     box_size_cmpc,
     field_volume,
     geometry="cube",
 ):
     """ 
-    How we assign halos to each field:
+    How we assign subhalos to each field:
     1. The simulation box is divided into non-overlapping cubes. Each cube is one field. The cube side length is chosen such that each cube has approximately the same volume as the target survey volume
-    2. Find all halos that are within the cube covered total volume (cubes don't fit in the total simulation volume perfectly)
-    3. For each halo position, divide by cube side length, and take the floor to define a cube index which this halo belongs to
-    4. Halos with the same cube are in the same field. A field can have zero, one or many halos
+    2. Find all subhalos that are within the cube covered total volume (cubes don't fit in the total simulation volume perfectly)
+    3. For each subhalo position, divide by cube side length, and take the floor to define a cube index which this halo belongs to
+    4. Subhalos with the same cube are in the same field. A field can have zero, one or many subhalos
     5. Flatten each cube index into a field id and count each repeated id within each mass bin (will do next) to build the count matrix
-    6. Fields with no halos are kept as zero rows for computation
+    6. Fields with no subhalos are kept as zero rows for computation
     """
     cube_side_cmpc = np.cbrt(field_volume.to_value(u.Mpc**3))
     n_side = int(np.floor(box_size_cmpc / cube_side_cmpc))
     covered_length = n_side * cube_side_cmpc
     n_fields=n_side**3
 
-    # Only keep halos inside the part of the simulation box tiled by full cubes.
+    # Only keep subhalos inside the part of the simulation box tiled by full cubes.
     inside = np.all(positions_cmpc < covered_length, axis=1)
     field_coord = np.floor(positions_cmpc[inside] / cube_side_cmpc).astype(int)
 
@@ -126,42 +127,46 @@ def assign_halos_to_fields(
     print("cube side:", cube_side_cmpc, "cMpc")
     print("cubes per dimension:", n_side)
     print("total fields:", n_fields)
-    print("fields with at least one halo:", len(np.unique(field_ids)))
+    print("fields with at least one subhalo:", len(np.unique(field_ids)))
     print("empty fields:", n_fields - len(np.unique(field_ids)))
 
     return field_ids, inside, cube_side_cmpc, n_fields
 
-def assign_halos_to_mass_bins(m200c_msun, inside, mass_bin_width):
+def assign_subhalos_to_mass_bins(mass_msun, inside, mass_bin_width):
     """
-    How we assign halos to mass bins:
-    1. For each halo inside the covered volume, we use its M200c mass in solar masses and bin them in log10 mass
+    How we assign subhalo to mass bins (we only use the stellar mass of subhalos here):
+    1. For each subhalo inside the covered volume, we use its stellar mass in solar masses and bin them in log10 mass
     2. A bin width of 0.5 dex means each bin is 0.5 wide in log10(M/Msun). This mean each bin is wider than the previous one by a factor of 10**0.5 ≈ 3.16
-    3. Each halo is assigned to exactly one mass bin. We use half-open bins: [lower edge, upper edge). A halo exactly on an upper edge is assigned to the next bin
-    4. A single mass bin can have multiple halos
-    5. We combine field ids and mass-bin ids to build the count matrix: count_matrix[field_id, mass_bin_id] gives the number of halos in that field and mass bin
+    3. Each subhalo is assigned to exactly one mass bin. We use half-open bins: [lower edge, upper edge). A subhalo exactly on an upper edge is assigned to the next bin
+    4. A single mass bin can have multiple subhalos
+    5. We combine field ids and mass-bin ids to build the count matrix: count_matrix[field_id, mass_bin_id] gives the number of subhalos in that field and mass bin
     """
-    masses_inside = m200c_msun[inside]
+    masses_inside = mass_msun[inside]
     log_mass = np.log10(masses_inside)
 
     log_mass_min = np.floor(log_mass.min() / mass_bin_width) * mass_bin_width #cleaner, integer edges
     log_mass_max = np.ceil(log_mass.max() / mass_bin_width) * mass_bin_width
 
     mass_bin_edges = np.arange(log_mass_min,log_mass_max + mass_bin_width,mass_bin_width,)
+    # mass_bin_edges = np.linspace(log_mass_min,log_mass_max, 100,)
     n_mass_bins = len(mass_bin_edges) - 1 #total bins. Must be 1 less than the number of edges
-    mass_bin_ids = np.full(len(log_mass), -1, dtype=int) #all mass bin ids of halos start with -1, meaning unassigned
-    #goes through each bin and find all halo masses in this bin instead of the opposite more better efficiency
+    mass_bin_ids = np.full(len(log_mass), -1, dtype=int) #all mass bin ids of subhalos start with -1, meaning unassigned
+    #goes through each bin and find all subhalo masses in this bin instead of the opposite more better efficiency
     for bin_id in range(n_mass_bins):
         lower_edge = mass_bin_edges[bin_id]
         upper_edge = mass_bin_edges[bin_id + 1]
         in_this_bin = ((log_mass >= lower_edge)& (log_mass < upper_edge))
         mass_bin_ids[in_this_bin] = bin_id
+    
+    if np.any(log_mass==mass_bin_edges[-1]): #boundary cases
+        mass_bin_ids[log_mass==mass_bin_edges[-1]]=n_mass_bins-1
 
-    in_mass_range = mass_bin_ids >= 0 #makes sure every halo we work with is assigned a bin
+    in_mass_range = mass_bin_ids >= 0 #makes sure every subhalo we work with is assigned a bin
 
     print("log mass range:", log_mass_min, log_mass_max)
     print("mass bin edges:", mass_bin_edges)
     print("number of mass bins:", n_mass_bins)
-    print("halos with assigned mass bin:", in_mass_range.sum())
+    print("subhalos with assigned mass bin:", in_mass_range.sum())
 
     return mass_bin_ids, in_mass_range, mass_bin_edges, n_mass_bins
 
@@ -173,8 +178,8 @@ def build_count_matrix(field_ids, mass_bin_ids, in_mass_range, n_fields, n_mass_
         count_matrix[field_id, mass_bin_id] += 1
 
     print("count matrix shape:", count_matrix.shape)
-    print("total counted halos:", count_matrix.sum())
-    print("halos used:", in_mass_range.sum())
+    print("total counted subhalos:", count_matrix.sum())
+    print("subhalos used:", in_mass_range.sum())
     assert count_matrix.sum() == in_mass_range.sum()
 
     return count_matrix
@@ -183,7 +188,7 @@ base_path = "/orcd/data/mvogelsb/005/Lumina/Lumina_above_z_4p75/group_files"
 snap_num = 36
 data = load_data(base_path, snap_num)
 
-positions_cmpc, m200c_msun, valid = select_valid_halos(data=data,res_cut=res_cut,)
+positions_cmpc, mass_msun, valid = select_valid_subhalos(data=data,res_cut=res_cut,stellar_mass_cut=stellar_mass_cut,)
 
 #box splitting
 field_volume, field_volume_error, los_depth = calculate_field_volume(cosmology=lumina_cosmology,z_center=data["redshift"],delta_z=delta_z,survey_area=survey_area,)
@@ -191,8 +196,8 @@ print("field volume:", field_volume)
 print("field volume error",field_volume_error)
 print("los depth", los_depth)
 
-field_ids, inside, cube_side_cmpc, n_fields = assign_halos_to_fields(positions_cmpc=positions_cmpc,box_size_cmpc=data["box_size_cmpc"],field_volume=field_volume,geometry=geometry,)
-mass_bin_ids, in_mass_range, mass_bin_edges, n_mass_bins = assign_halos_to_mass_bins(m200c_msun=m200c_msun,inside=inside,mass_bin_width=mass_bin_width,)
+field_ids, inside, cube_side_cmpc, n_fields = assign_subhalos_to_fields(positions_cmpc=positions_cmpc,box_size_cmpc=data["box_size_cmpc"],field_volume=field_volume,geometry=geometry,)
+mass_bin_ids, in_mass_range, mass_bin_edges, n_mass_bins = assign_subhalos_to_mass_bins(mass_msun=mass_msun,inside=inside,mass_bin_width=mass_bin_width,)
 count_matrix = build_count_matrix(field_ids=field_ids,mass_bin_ids=mass_bin_ids,in_mass_range=in_mass_range,n_fields=n_fields,n_mass_bins=n_mass_bins,)
 
 def compute_cosmic_variance(count_matrix, mass_bin_edges):
@@ -230,7 +235,8 @@ def compute_cosmic_variance(count_matrix, mass_bin_edges):
             f"mean={mean_count:.4g}, "
             f"variance={total_variance:.4g}, "
             f"poisson={poisson_variance:.4g}, "
-            f"sigma_cv={sigma_cv:.4g}"
+            f"sigma_cv={sigma_cv:.4g},"
+            f"total galaxies in bin={counts.sum()}"
         )
 
     return {
@@ -244,10 +250,35 @@ def compute_cosmic_variance(count_matrix, mass_bin_edges):
 """ first diagnostic plot"""
 cv_results = compute_cosmic_variance(count_matrix=count_matrix,mass_bin_edges=mass_bin_edges,)
 mass_bin_centers = 0.5 * (mass_bin_edges[:-1] + mass_bin_edges[1:])
-plt.figure(figsize=(7, 5))
-plt.plot(mass_bin_centers,cv_results["sigma_cv"],marker="o",linestyle="-",)
-plt.xlabel(r"$\log_{10}(M_{200c}/M_\odot)$")
-plt.ylabel(r"$\sigma_{\rm CV}$")
-plt.title("Cosmic variance of the HMF")
-plt.grid(alpha=0.3)
-plt.savefig("sigma_cv_vs_mass.png", dpi=200, bbox_inches="tight")
+min_total_objects = 10
+total_objects_per_bin = count_matrix.sum(axis=0)
+ok = total_objects_per_bin >= min_total_objects
+
+output_dir = Path("outputs")
+# plt.figure(figsize=(7, 5))
+# plt.plot(mass_bin_centers[ok],cv_results["sigma_cv"][ok],marker="o",linestyle="-",)
+# plt.xlabel(r"$\log_{10}(M_{\mathrm{star}}(<2R_{1/2})/M_\odot)$")
+# plt.ylabel(r"$\sigma_{\rm CV}$")
+# plt.title("Cosmic variance of the SMF")
+# plt.grid(alpha=0.3)
+# plt.savefig(output_dir / "sigma_cv_vs_mass_SMF.png", dpi=200, bbox_inches="tight")
+
+#compute SMF to compare with observational data
+total_galaxies_per_bin = count_matrix.sum(axis=0)
+volume = n_fields * field_volume.to_value(u.Mpc**3)
+phi = total_galaxies_per_bin / (volume * mass_bin_width)
+log_phi = np.log10(phi)
+
+from scripts.plot_obs_smf import plot_obs_smf
+
+fig, ax = plt.subplots(figsize=(7, 5))
+
+ax.plot(mass_bin_centers,log_phi,marker="o",linestyle="-",color="tab:blue",label="Lumina",)
+
+plot_obs_smf(redshift=12,ax=ax,dict_style={"ms": 5, "color": "black"},)
+ax.set_xlabel(r"$\log_{10}(M_{\mathrm{star}}/M_\odot)$")
+ax.set_ylabel(r"$\log_{10}(\Phi / {\rm Mpc}^{-3}\,{\rm dex}^{-1})$")
+ax.set_title("Stellar Mass Function")
+ax.grid(alpha=0.3)
+ax.legend()
+plt.savefig(output_dir / "SMF_lumina_vs_obs.png",dpi=200,bbox_inches="tight",)
--- scripts/plot_obs_smf.py
+++ scripts/plot_obs_smf.py
@@ -9,65 +9,65 @@ def plot_obs_smf(redshift, ax, dict_style={}):
         f = np.genfromtxt("./obdata/SMF/Santini2012_z3.dat", names=True)
         ax.errorbar(f['logMstar'] + np.log10(factor_from_Salpeter_to_Chabrier), f['logPhi'],
             yerr=( f['logPhi'] - f['lo'], f['hi'] - f['logPhi']),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1)#, label=r"$\rm Santini+12$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1), #label=r"$\rm Santini+12$" if redshift==3 else None)
 
     if redshift == 3:
         f = np.genfromtxt("./obdata/SMF/llbert2013.dat", names=True) # Chabrier IMF
         ax.errorbar(f['logMstar'], f['logPhi'],
             yerr=( f['logPhi'] - f['lo'], f['hi'] - f['logPhi']),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1)#, label=r"$\rm Ilbert+13$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1), #label=r"$\rm Ilbert+13$" if redshift==3 else None)
 
         f = np.genfromtxt("./obdata/SMF/Davidzon2017.dat", names=True) # Chabrier IMF
         ax.plot(f['logMstar'], f['logPhi'],
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2,  zorder=-1)#, label=r"$\rm Davidzon+17$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2,  zorder=-1), #label=r"$\rm Davidzon+17$" if redshift==3 else None)
 
     f = np.genfromtxt("./obdata/SMF/Song2016.dat", names=True) # Salpeter IMF
     sel = f['z'] == redshift
     ax.errorbar(f['logMstar'][sel] + np.log10(factor_from_Salpeter_to_Chabrier), f['logPhi'][sel],
              yerr=(f['loerr'][sel], f['hierr'][sel]), marker='o', ms=dict_style['ms'],
-             linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2,  zorder=-1)#, label=r"$\rm Song+16$" if redshift==3 else None)
+             linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2,  zorder=-1),#label=r"$\rm Song+16$" #if redshift==3 else None)
 
     f = np.genfromtxt("./obdata/SMF/Stefanon2021.dat", names=True) # Salpeter IMF
     sel = f['z'] == redshift
     ax.errorbar(f['logMstar'][sel] + np.log10(factor_from_Salpeter_to_Chabrier), np.log10(f['Phi']*1e-4)[sel],
             yerr=( np.log10(f['Phi'])[sel] - np.log10(f['Phi']-f['loerr'])[sel], np.log10(f['Phi']+f['hierr'])[sel]-np.log10(f['Phi'])[sel]),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1)#, label=r"$\rm Stefanon+21$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1),# label=r"$\rm Stefanon+21$" #if redshift==3 else None)
 
     f = np.genfromtxt("./obdata/SMF/Weaver2023.dat", names=True) # Chabrier IMF
     sel = f['z'] == redshift
     ax.errorbar(f['logMstar'][sel], f['logPhi'][sel],
             yerr=( f['logPhi'][sel] - f['lo'][sel], f['hi'][sel] - f['logPhi'][sel]),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1)#, label=r"$\rm Weaver+23$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1),#label=r"$\rm Weaver+23$" #if redshift==3 else None)
 
     f = np.genfromtxt("./obdata/SMF/Shuntov2025.dat", names=True)
     sel = f['z'] == redshift
     ax.errorbar(f['logMstar'][sel], f['logPhi'][sel],
             yerr=( f['logPhi'][sel] - f['lo'][sel], f['hi'][sel] - f['logPhi'][sel]),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1)#, label=r"$\rm Shuntov+25$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1),# label=r"$\rm Shuntov+25$" #if redshift==3 else None)
 
     ## JWST
     f = np.genfromtxt("./obdata/SMF/Wiebel2024.dat", names=True) # Kroupa IMF
     sel = (f['zmed'] == redshift) & (f['logPhi_loerr']<1e10)
     ax.errorbar(f['logMstar'][sel] + np.log10(factor_from_Kroupa_to_Chabrier), f['logPhi'][sel],
              yerr=(f['logPhi_loerr'][sel], f['logPhi_uperr'][sel]), marker='o', ms=dict_style['ms'],
-             linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2,  zorder=-1)#, label=r"$\rm Weibel+24$" if redshift==3 else None)
+             linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2,  zorder=3), #label=r"$\rm Weibel+24$" #if redshift==3 else None)
     sel = (f['zmed'] == redshift) & (f['logPhi_loerr']>1e10)
     ax.errorbar(f['logMstar'][sel] + np.log10(factor_from_Kroupa_to_Chabrier), f['logPhi'][sel],
              yerr=(0.3*np.ones(len(f['logMstar'][sel])), np.zeros(len(f['logMstar'][sel]))), marker='o', ms=dict_style['ms'],
-             linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=1,  zorder=-1, uplims=True)
+             linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=1,  zorder=3, uplims=True)
 
     f = np.genfromtxt("./obdata/SMF/Harvey2025.dat", names=True)
     sel = (f['zmin'] == redshift - 0.5) & (f['Phi']-f['Phi_loerr']>0)
     ax.errorbar(f['logMstar'][sel] + np.log10(factor_from_Kroupa_to_Chabrier), np.log10(f['Phi']*1e-4)[sel],
             yerr=( np.log10(f['Phi'][sel]) - np.log10(f['Phi']-f['Phi_loerr'])[sel], np.log10(f['Phi']+f['Phi_uperr'])[sel] - np.log10(f['Phi'][sel])),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1)#, label=r"$\rm Harvey+25$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=3, label=r"$\rm Harvey+25$") #if redshift==3 else None)
     sel = (f['zmin'] == redshift - 0.5) & (f['Phi']-f['Phi_loerr']==0)
     ax.errorbar(f['logMstar'][sel] + np.log10(factor_from_Kroupa_to_Chabrier), np.log10(f['Phi']*1e-4)[sel],
             yerr=( 0.3 * np.ones(len(f['logMstar'][sel])), np.log10(f['Phi']+f['Phi_uperr'])[sel] - np.log10(f['Phi'][sel])),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=1, zorder=-1, uplims=True)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=1, zorder=3, uplims=True)
 
     f = np.genfromtxt("./obdata/SMF/Wang2025.dat", names=True)
     sel = f['z'] == redshift
     ax.errorbar(f['logMstar'][sel] + np.log10(factor_from_Kroupa_to_Chabrier), f['logPhi'][sel],
             yerr=( f['logPhi'][sel] - f['lo'][sel], f['hi'][sel] - f['logPhi'][sel]),
-            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=-1)#, label=r"$\rm Wang+25$" if redshift==3 else None)
+            marker='o', ms=dict_style['ms'], linestyle='none', color=dict_style['color'], mfc=dict_style['color'], mew=2, mec=dict_style['color'], lw=2, capsize=4, capthick=2, zorder=3), #label=r"$\rm Wang+25$" #if redshift==3 else None)
